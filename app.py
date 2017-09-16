e53661d2e190912a19fd9a0956775b1821b9762d
Samuel Breese <samuel@chame.co>
2017-09-16 13:11:45 -0400

    Add pylint dependency, make app.py pep8 compliant

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-app = Flask(__name__)
+"""
+Traveling Customer
+"""
 
-@app.route("/")
+from flask import Flask
+APP = Flask(__name__)
+
+@APP.route("/")
 def hello_world():
+    """
+    Placeholder route
+    """
     return "Hello, world!"
 
 if __name__ == "__main__":
-    app.run()
+    APP.run()
