9d00ce4de4c91287e2d8151fde7f872553488a61
DefectiveMan <pelaet@rpi.edu>
2017-10-25 11:37:37 -0400

    Stop complaining about linebreaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         "react/forbid-prop-types": 0,
         "react/prefer-stateless-function": 0,
-        "react/prop-types": 0
+        "react/prop-types": 0,
+        "linebreak-style": 0,
      }
    };
