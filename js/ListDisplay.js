fd2c95169d1aef9614dd64f7608196dc060a1c4e
Melissa Lindquist <mlsslndqst@gmail.com>
2017-11-26 17:08:48 -0500

    added comments

--- js/ListDisplay.js
+++ js/ListDisplay.js
@@ -1,47 +1,15 @@
 import React, { Component } from 'react';
-import CommunicationButton from './CommunicationButton';
 import ListItem from './ListItem';
-// import DropdownTime from './dropdownTime';
 import styles from '../CSS/Thumbnail.css';
 
+/*
+Requires: list of items
+Displays list of items as ListItem objects
+*/
 class ListDisplay extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      // items: this.props.items,
-    };
-    this.updateList = this.updateList.bind(this);
-    this.handleRecieveInfo = this.handleRecieveInfo.bind(this);
-  }
-  updateList(newItem) {
-    console.log('updateList');
-    // this.state.items[newItem.name] = newItem.id;
-    this.setState({ items: this.state.items.concat([newItem]) });
-    this.props.map.updateLocationList(newItem);
-  }
-  handleRecieveInfo(info) {
-    console.log('handleRecieveInfo', info.path);
-    this.setState({ items: info.optimal.path });
-    this.props.map.changeLocationList(info.optimal.path.slice(1, info.optimal.path.length - 1));
-  }
   render() {
     console.log('render list display props: ', this.props);
     const list = this.props.items;
-    /*
-    const currentLocation = this.props.map.state.center;
-    const startTime = (<DropdownTime spec="Start Time" />);
-    const endTime = (<DropdownTime spec="End Time" />);
-    console.log('render ', x);
-    const info = { places: list, start_loc: currentLocation };
-    const comButton =
-      (<CommunicationButton
-        sendInfo={info}
-        onRecieve={this.handleRecieveInfo}
-        text="Done"
-      />);
-      */
-    // Dead code: <li key={item.id}>{item.name}</li>
     return (
       <div style={styles.list}>
         <ul>
@@ -56,12 +24,5 @@ class ListDisplay extends Component {
 }
 ListDisplay.defaultProps = {
   items: [],
-
 };
 export default ListDisplay;
-// const places = {props.items.map((item =>
-//   <div key={item.name}>
-//     <h3>{item.name}</h3>
-//     <p>{item.id}</p>
-//   </div>
-// )};
